ba4c34707380233cbc02864bf552cb2f9b3d32fb
Johnathon Biersack <Johnathon Biersack>
2017-03-25 18:13:01 -0500

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,12 @@ import { HomeComponent } from './components/home/home.component';
 import { IosComponent } from './components/ios/ios.component';
 import { AndroidComponent } from './components/android/android.component';
 import {routing} from './app.routing';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, routing, FormsModule, HttpModule ],
-  declarations: [ AppComponent, NavbarComponent, HomeComponent, IosComponent, AndroidComponent ],
+  declarations: [ AppComponent, NavbarComponent, HomeComponent, IosComponent, AndroidComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
